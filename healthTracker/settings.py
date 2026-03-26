deb53fa613762f58bc0943737e6fe209aace3ee6
TJHuck <78052758+TJHuck@users.noreply.github.com>
2026-03-26 13:55:40 -0400

    Add accounts app with user registration and authentication features. Working Login, Admin, Signup pages, as well as functioning sign out.

--- healthTracker/settings.py
+++ healthTracker/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -125,4 +126,7 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_REDIRECT_URL = "/"
+LOGIN_REDIRECT_URL = "home"
+LOGOUT_REDIRECT_URL = "home"
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'

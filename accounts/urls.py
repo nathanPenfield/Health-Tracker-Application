1d9fc42be9c5a1363cf71b16ad2edea19aa4bf59
nathanPenfield <nathanpenfield3@gmail.com>
2026-03-30 17:14:05 -0400

    custom user form

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from .views import SignUpView
+from . import views
 
 urlpatterns = [
-    path("signup/", SignUpView.as_view(), name="signup"),
+    path("signup/", views.SignUpView, name="signup"),
+    path("login/", views.LoginView,name="login"),
+    path("signout/", views.SignOutView, name= "signout"),
 ]

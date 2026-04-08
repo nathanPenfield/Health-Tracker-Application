56c50fdb893d621a091e7ca71e3cf9fe29f00f30
TJHuck <78052758+TJHuck@users.noreply.github.com>
2026-04-08 11:53:33 -0400

    Feat/ Implement medication entry functionality with models, views, forms, and admin integration.

---
+++ medication_entry/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class MedicationEntryConfig(AppConfig):
+    name = 'medication_entry'
---
+++ medication_entry/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render, redirect
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import CreateView, ListView
+from .models import Medication
+from .forms import MedicationForm
+
+class MedicationCreateView(LoginRequiredMixin, CreateView):
+    model = Medication
+    form_class = MedicationForm
+    template_name = 'medication_entry/medication_form.html'
+    
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+
+class MedicationListView(LoginRequiredMixin, ListView):
+    model = Medication
+    template_name = 'medication_entry/medication_list.html'
+    context_object_name = 'medications'
+    
+    def get_queryset(self):
+        return Medication.objects.filter(user=self.request.user)

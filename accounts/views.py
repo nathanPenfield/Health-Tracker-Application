465d7ea98f2ab818d9051af2a02e690bf0dd1178
pm11-ai <pm11@albion.edu>
2026-04-08 15:36:21 -0400

    added a way to try and take the medication list onto the window

--- accounts/views.py
+++ accounts/views.py
@@ -3,8 +3,10 @@ from .forms import CustomUserCreationForm
 from django.contrib.auth.forms import AuthenticationForm
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
-from django.views.decorators.http import require_POST
+from django.views.decorators.http import require_http_methods
 from django.http import HttpRequest, HttpResponse
+#from .models import Medication
+#from .forms import MedicationForm
 
 def SignUpView(request: HttpRequest) -> HttpResponse:
     if request.user.is_authenticated:
@@ -36,10 +38,29 @@ def LoginView(request: HttpRequest) -> HttpResponse:
     return render(request, "registration/login.html", {"form": form})
     
 @login_required
-@require_POST
+@require_http_methods
 def SignOutView(request: HttpRequest) -> HttpResponse:
     logout(request)
     return redirect("/")
+
+#@login_required
+#@require_http_methods(["GET", "POST"])
+#def MedicationView(request: HttpRequest) -> HttpResponse:
+    if request.method == "POST":
+        form = MedicationForm(request.POST)
+        if form.is_valid():
+            med = form.save(commit=False)
+            med.user = request.user
+            med.save()
+            return redirect("/")
+    else:
+        form = MedicationForm()
+    logs = Medication.Object.filter(user=request.user)
+
+    return render(request, "home.html", {
+        "logs": logs,
+        "form": form
+    })
 
 
 """

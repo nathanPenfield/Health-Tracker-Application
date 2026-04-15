f34003d88489fc9f96b50d30e31d6e604a785812
pm11-ai <pm11@albion.edu>
2026-04-15 13:18:45 -0400

    added medication log to dropdown menu and fixed medication list to display mediction from the form

--- accounts/views.py
+++ accounts/views.py
@@ -5,8 +5,6 @@ from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_http_methods
 from django.http import HttpRequest, HttpResponse
-#from .models import Medication
-#from .forms import MedicationForm
 
 def SignUpView(request: HttpRequest) -> HttpResponse:
     if request.user.is_authenticated:
@@ -42,25 +40,6 @@ def LoginView(request: HttpRequest) -> HttpResponse:
 def SignOutView(request: HttpRequest) -> HttpResponse:
     logout(request)
     return redirect("/")
-
-#@login_required
-#@require_http_methods(["GET", "POST"])
-#def MedicationView(request: HttpRequest) -> HttpResponse:
-    if request.method == "POST":
-        form = MedicationForm(request.POST)
-        if form.is_valid():
-            med = form.save(commit=False)
-            med.user = request.user
-            med.save()
-            return redirect("/")
-    else:
-        form = MedicationForm()
-    logs = Medication.Object.filter(user=request.user)
-
-    return render(request, "home.html", {
-        "logs": logs,
-        "form": form
-    })
 
 
 """
--- medication_entry/views.py
+++ medication_entry/views.py
@@ -3,11 +3,13 @@ from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import CreateView, ListView
 from .models import Medication
 from .forms import MedicationForm
+from django.urls import reverse_lazy
 
 class MedicationCreateView(LoginRequiredMixin, CreateView):
     model = Medication
     form_class = MedicationForm
     template_name = 'medication_entry/medication_form.html'
+    success_url = reverse_lazy('medication_list')
     
     def form_valid(self, form):
         form.instance.user = self.request.user
